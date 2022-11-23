5f437f0ae6029f374113dd0e946841ea60f2fd3f
Hemali Solanki <100782515+Hemali1310@users.noreply.github.com>
2022-11-23 16:31:34 +0000

    day4 challenge

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print('Hello World')
+import random
+import pi
 
-print("Hello " + input("What is your name? "))
+randomint = random.randint(23,78)
+print(randomint)
+
+randomfloat = random.random()
+print(randomfloat)
+
+print(pi.pi)
+
+listdata = ["hemali", 1, 3, 5, "aman", True, False, 5]
+print(listdata[0])
+print(listdata[-1])
+
+listdata[2] = 9
+print(listdata)
