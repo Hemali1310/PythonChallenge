7697a9a89deb58bad3c73e0308fde74783f459f9
Hemali Solanki <100782515+Hemali1310@users.noreply.github.com>
2022-11-23 16:46:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+def greet():
+  print("Hi, huh")
+  print("Hello")
+  print("Hey")
+
+# greet()
+def greetname(name):
+  print(f"enter your name: {name}")
+
+greetname("hemali")
+
+num = int(input("Enter a number: "))
+if num > 1:
+  for i in range(2,num):
+    if (num % i) == 0:
+      print(num,"is not a prime number")
+      break
+    else:
+      print(num,"is a prime number")
+else:
+   print(num,"is not a prime number")
