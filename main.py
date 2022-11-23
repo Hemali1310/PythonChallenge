6c442c46556c893467fc38b56056f2b2861dc83e
Hemali Solanki <100782515+Hemali1310@users.noreply.github.com>
2022-11-23 19:08:36 +0000

    calculator in pythom

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
-print('Hello World')
+def sum(a, b):
+  return a + b
+  
+def minus(a, b):
+  return a - b
 
-print("Hello " + input("What is your name? "))
+def multiply(a, b):
+  return a * b
+
+def division(a, b):
+ return a / b
+
+def operations(fnum, snum):
+  if operation == '+':
+    return sum(firstnum, secondnum)
+  if operation == '-':
+    return minus(firstnum, secondnum)
+  if operation == '*':
+    return multiply(firstnum, secondnum)
+  if operation == '/':
+    return division(firstnum, secondnum)
+
+
+firstnum = int(input("Enter first num: "))
+print("+ \n- \n* \n/")
+operation = input("Pick an operation: ")
+secondnum = int(input("Enter second num: "))
+output = operations(firstnum, secondnum)
+print(output)
+nextstep = input(
+  f"press y for continue with {output} and n for starting over: ")
+while (nextstep == 'y'):
+  firstnum = output
+  print("+ \n- \n* \n/")
+  operation = input("Pick an operation: ")
+  secondnum = int(input("Enter second num: "))
+  output = operations(firstnum, secondnum)
+  print(output)
+  nextstep = input(
+    f"press y for continue with {output} and n for starting over: ")
