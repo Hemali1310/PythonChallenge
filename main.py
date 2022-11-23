9b23ecbcae36a12a63618dc9a49bda3d0e7bca8e
Hemali Solanki <100782515+Hemali1310@users.noreply.github.com>
2022-11-23 19:04:28 +0000

    day10 challenge

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-print('Hello World')
+def firstcapital(fname, lname):
+  firstname = fname.title()
+  lastname = lname.capitalize()
+  print(firstname)
+  print(lastname)
 
-print("Hello " + input("What is your name? "))
+fname = input("Enter first name: ")
+lname = input("Enter last name: ")
+firstcapital(fname, lname)
+
+# def my_function(a):
+#   if a < 40:
+#     return
+#     print("Terrible")
+#   if a < 80:
+#     return "Pass"
+#   else:
+#     return "Great"
+# print(my_function(25))
